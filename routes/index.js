b6648dd7122c3a5520069fef1db2d0a51749c69c
Silin7 <silin7@foxmail.com>
2026-03-22 16:35:09 +0800

    删除冗余代码

--- routes/index.js
+++ routes/index.js
@@ -7,27 +7,13 @@
 const express = require("express");
 const Router = express.Router();
 
-const babyRouter = require("./components/baby-router");
-const familyRouter = require("./components/family-router");
 const loginRouter = require("./components/login-router");
 const systemRouter = require("./components/system-router");
-const babyFeedingRouter = require("./components/baby-feeding-router");
-const babyMedicationRouter = require("./components/baby-medication-router");
-const babyPoopRouter = require("./components/baby-poop-router");
-const babySleepRouter = require("./components/baby-sleep-router");
-const babyTemperatureRouter = require("./components/baby-temperature-router");
 const websitePagesRouter = require("./components/website-pages-router");
 
 Router
-  .use(babyRouter)
-  .use(familyRouter)
   .use(loginRouter)
   .use(systemRouter)
-  .use(babyFeedingRouter)
-  .use(babyMedicationRouter)
-  .use(babyPoopRouter)
-  .use(babySleepRouter)
-  .use(babyTemperatureRouter)
   .use(websitePagesRouter)
 
 module.exports = Router;
